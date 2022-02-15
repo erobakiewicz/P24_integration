d75ea48f4cbf388130b8bcea5b671118a8807fe1
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-02-15 17:54:59 +0100

    psycopg2 installed

--- P24_integration/settings.py
+++ P24_integration/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'P24_integration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR + '/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
